a7cc973a30229c3a497019f3bb8aff94f1c3ccf1
Alexandr Bobko <sasha.bobcko@gmail.com>
2017-03-20 20:35:51 +0400

    First commit

---
+++ attractions_db/authorize.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Авторизация</title>
+</head>
+<body>
+	<h1>Авторизация</h1>
+	<form method = 'POST'>
+		<p><input type="text" name="login"> Введите логин</p>
+		<p><input type="password" name="password"> Введите пароль</p>
+		<p><input type="submit" name="authorize" value = "Войти">
+	</form>
+</body>
+</html>
---
+++ attractions_db/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>База данных</title>
+</head>
+<body>
+<h1>База данных "Развлечения"</h1>
+
+<ul>
+<li><a href = "authorize.php">Авторизация</a></li>	
+</ul>
+<!--	<?php
+		$mysqli = new mysqli('localhost','root', '', 'library');
+		if ($mysqli->connect_error)
+			die ('Connect error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+		else echo 'присоединился';
+		
+	?> -->
+
+</body>
+</html>
